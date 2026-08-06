a6bcb53c760d4fd3080862647bf037a6579dbe6e
poojan <poojan16924@gmail.com>
2026-08-06 11:50:15 +0530

    Initial commit

---
+++ type_data.py
@@ -0,0 +1,23 @@
+# 
+
+# a=10.5
+# print(a,type(a),id(a))
+
+
+# a=10.5j
+# print(a,type(a),id(a))
+
+# bool(0)
+# print(bool(0))
+
+# bool(1)
+# print(bool(1))
+
+
+# print(bool("hi"))
+
+# print(bool(1000))
+
+# print(bool(""))
+
+print(bool(" "))
